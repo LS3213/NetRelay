4e016c9fcb67268c770381de2436f924a62f747b
lansi <lansi4737@outlook.com>
2026-06-12 01:41:47 +0800

    fix: refine native title bar controls

--- src/NetRelay/MainWindow.xaml.cs
+++ src/NetRelay/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 using System.Windows;
+using System.Windows.Media;
 using NetRelay.Native;
 using NetRelay.Services;
 using NetRelay.ViewModels;
@@ -12,11 +13,30 @@ public partial class MainWindow : Window
         InitializeComponent();
         DataContext = new MainViewModel(new NetworkAdapterService());
         SourceInitialized += (_, _) => WindowBackdrop.Apply(this);
+        StateChanged += (_, _) => UpdateMaximizeIcon();
+        UpdateMaximizeIcon();
+    }
+
+    private void MinimizeButton_Click(object sender, RoutedEventArgs e)
+    {
+        WindowState = WindowState.Minimized;
+    }
+
+    private void MaximizeButton_Click(object sender, RoutedEventArgs e)
+    {
+        WindowState = WindowState == WindowState.Maximized ? WindowState.Normal : WindowState.Maximized;
     }
 
     private void CloseButton_Click(object sender, RoutedEventArgs e)
     {
         Close();
     }
-}
 
+    private void UpdateMaximizeIcon()
+    {
+        MaximizeIcon.Data = Geometry.Parse(
+            WindowState == WindowState.Maximized
+                ? "M 3,1 L 10,1 L 10,8 M 1,3 L 8,3 L 8,10 L 1,10 Z"
+                : "M 1,1 L 10,1 L 10,10 L 1,10 Z");
+    }
+}
